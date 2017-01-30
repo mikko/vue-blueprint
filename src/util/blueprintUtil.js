7bd399a49a2984a066ee3bb361128a6b4bc2a2d9
Mikko Pohja <mikko.pohja@futurice.com>
2017-01-30 17:43:07 +0200

    Separate utils

---
+++ src/util/blueprintUtil.js
@@ -0,0 +1,27 @@
+import * as d3 from 'd3';
+
+const coordToLine = d3.line()
+  .x(coord => coord[0])
+  .y(coord => coord[1]);
+
+const coordPairsToLine = cPairs => cPairs.map(pair => coordToLine([pair[0], pair[1]])).join('');
+
+const sqr = x => x * x;
+const dist2 = (v, w) => sqr(v[0] - w[0]) + sqr(v[1] - w[1]);
+const distToSegmentSquared = (p, v, w) => {
+  const l2 = dist2(v, w);
+  if (l2 === 0) {
+    return dist2(p, v);
+  }
+  let t = (((p[0] - v[0]) * (w[0] - v[0])) + ((p[1] - v[1]) * (w[1] - v[1]))) / l2;
+  t = Math.max(0, Math.min(1, t));
+  return dist2(p, [v[0] + (t * (w[0] - v[0])), v[1] + (t * (w[1] - v[1]))]);
+};
+const distanceToSegment = (p, w, v) => Math.sqrt(distToSegmentSquared(p, v, w));
+
+
+export default {
+  coordToLine,
+  coordPairsToLine,
+  distanceToSegment,
+};
